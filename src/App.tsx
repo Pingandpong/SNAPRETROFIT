de19ae1be78bb2675f0996f7a131dcfa7e797095
Pingandpong <deploy0905@gmail.com>
2025-07-21 23:26:51 +0900

    Add Firebase setup and React Navigation

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './config/firebaseConfig'; // Initialize Firebase
+import AppNavigator from './navigation/AppNavigator';
+
+export default function App() {
+  return (
+    <AppNavigator />
+  );
+}
